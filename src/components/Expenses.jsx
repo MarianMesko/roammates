69ef0f83a79d26c0a1799f4473c240a1050003a0
MarianMesko <majomesko16@gmail.com>
2025-06-14 11:52:00 +0200

    Create Expenses.jsx

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,8 @@
+export default function Expenses() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-xl font-bold">Expenses</h2>
+      <p>Expenses will go here</p>
+    </div>
+  )
+}
