88f0842ad425b01d5cb03961e3692a9bc608e01a
MarianMesko <majomesko16@gmail.com>
2025-06-14 11:50:38 +0200

    Update Polls.jsx

--- src/components/Polls.jsx
+++ src/components/Polls.jsx
@@ -1,1 +1,9 @@
+export default function Polls() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-xl font-bold">Activity Polls</h2>
+      <p>Polls will go here</p>
+    </div>
+  )
+}
 
