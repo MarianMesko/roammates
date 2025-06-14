63b137b7cd448330c786f2a59f547ecb6b250ec5
MarianMesko <majomesko16@gmail.com>
2025-06-14 11:29:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Chat from './pages/Chat.jsx'
+import Polls from './pages/Polls.jsx'
+import Expenses from './pages/Expenses.jsx'
+import Photos from './pages/Photos.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/polls" element={<Polls />} />
+      <Route path="/expenses" element={<Expenses />} />
+      <Route path="/photos" element={<Photos />} />
+      <Route path="/home" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
