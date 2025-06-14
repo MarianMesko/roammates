5d8f735fe8c7b07fd2728b198166b7222d4bc4fa
MarianMesko <majomesko16@gmail.com>
2025-06-14 11:52:34 +0200

    Create Gallery.jsx

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-xl font-bold">Gallery</h2>
+      <p>Gallery will go here</p>
+    </div>
+  )
+}
