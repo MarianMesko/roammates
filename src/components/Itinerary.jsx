eab4f92784062fa0dbfc16b49088f0cc54909ec2
MarianMesko <majomesko16@gmail.com>
2025-06-14 11:51:21 +0200

    Create Itinerary.jsx

---
+++ src/components/Itinerary.jsx
@@ -0,0 +1,8 @@
+export default function Itinerary() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-xl font-bold">Itinerary</h2>
+      <p>Itinerary will go here</p>
+    </div>
+  )
+}
